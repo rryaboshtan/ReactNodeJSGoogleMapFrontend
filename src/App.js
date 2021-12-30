b394f13f7a237ddd6f503189db3a4533d09566a8
rryaboshtan <rrvworkroman@gmail.com>
2021-12-31 01:58:07 +0200

    install dotenv

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+
+import "@reach/combobox/styles.css"
 
 function App() {
+  const {} = useLoadScript({
+     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
    return <div>map</div>;
 }
 
