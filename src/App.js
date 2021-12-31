4d5ed2151ab9bba7c5e685ebf547a9b30a80cf2c
rryaboshtan <rrvworkroman@gmail.com>
2021-12-31 14:29:03 +0200

    add onMapLoad

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import React, { useState, useCallback, useRef } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 import './App.css';
 import '@reach/combobox/styles.css';
 import mapStyles from './mapStyles';
@@ -12,20 +12,23 @@ const options = {
 
 function App() {
    const [markers, setMarkers] = useState([]);
+   const [selected, setSelected] = useState(null);
 
-   const onMapClick = useCallback(
-      event => {
-         setMarkers(current => [
-            ...current,
-            {
-               lat: event.latLng.lat(),
-               lng: event.latLng.lng(),
-               time: new Date(),
-            },
-         ]);
-      },
-      [],
-   );
+   const onMapClick = useCallback(event => {
+      setMarkers(current => [
+         ...current,
+         {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+            time: new Date(),
+         },
+      ]);
+   }, []);
+
+   const mapRef = useRef();
+   const onMapLoad = useCallback(map => {
+      mapRef.current = map;
+   }, []);
 
    const MyMapComponent = withScriptjs(
       withGoogleMap(() => (
@@ -34,6 +37,7 @@ function App() {
             defaultCenter={{ lat: 43.653225, lng: -79.383186 }}
             options={options}
             onClick={onMapClick}
+            onLoad={onMapLoad}
          >
             {markers.map(marker => (
                <Marker
@@ -42,8 +46,25 @@ function App() {
                   icon={{
                      url: '/marker.png',
                   }}
+                  onClick={() => {
+                     setSelected(marker);
+                  }}
                ></Marker>
             ))}
+
+            {selected ? (
+               <InfoWindow
+                  position={{ lat: selected.lat, lng: selected.lng }}
+                  onCloseClick={() => {
+                     setSelected(null);
+                  }}
+               >
+                  <div>
+                     <h2>Bear spotted!</h2>
+                     {/* <p>Spotted {{ selected.time }}</p> */}
+                  </div>
+               </InfoWindow>
+            ): null}
          </GoogleMap>
       ))
    );
