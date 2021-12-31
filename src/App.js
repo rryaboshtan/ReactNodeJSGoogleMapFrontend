08a97a46d81ae5cf68aeb4feec2f587dc67bb6df
rryaboshtan <rrvworkroman@gmail.com>
2021-12-31 03:16:23 +0200

    Map works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from "react";
-import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
+import React from 'react';
+import '@reach/combobox/styles.css';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-import "@reach/combobox/styles.css"
+
+// const center = {
+//   lat: 43.653225,
+//   lng: -79.383186,
+// }
+
+const MyMapComponent = withScriptjs(
+   withGoogleMap(props => (
+      <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+         {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+      </GoogleMap>
+   ))
+);
 
 function App() {
-  const {} = useLoadScript({
-     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  });
-
-   return <div>map</div>;
+   return (
+      <div style={{ width: '100vw', height: '100vh' }}>
+         <MyMapComponent
+            isMarkerShown
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=places`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+         />
+      </div>
+   );
 }
 
 export default App;
