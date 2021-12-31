b68414b2e2189974eadc196e9b536c5ffd103f16
rryaboshtan <rrvworkroman@gmail.com>
2021-12-31 13:13:54 +0200

    show markers

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 import React, { useState } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import './App.css';
 import '@reach/combobox/styles.css';
 import mapStyles from './mapStyles';
-// const center = {
-//   lat: 43.653225,
-//   lng: -79.383186,
-// }
+
 const options = {
    styles: mapStyles,
    disableDefaultUI: true,
    zoomControl: true,
-}
-
-const MyMapComponent = withScriptjs(
-   withGoogleMap(props => (
-      <GoogleMap
-         defaultZoom={8}
-         defaultCenter={{ lat: 43.653225, lng: -79.383186 }}
-         options={options}
-         onClick={event => {
-            props.setMarkers(current => [
-               ...current,
-               {
-                  lat: event.latLng.lat(),
-                  lng: event.latLng.lng(),
-                  time: new Date(),
-               }
-               ])
-         }
-         
-         }
-      >
-         {/* {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />} */}
-      </GoogleMap>
-   ))
-);
+};
 
 function App() {
    const [markers, setMarkers] = useState([]);
+
+   const MyMapComponent = withScriptjs(
+      withGoogleMap(() => (
+         <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{ lat: 43.653225, lng: -79.383186 }}
+            options={options}
+            onClick={event => {
+               setMarkers(current => [
+                  ...current,
+                  {
+                     lat: event.latLng.lat(),
+                     lng: event.latLng.lng(),
+                     time: new Date(),
+                  },
+               ]);
+            }}
+         >
+            {markers.map(marker => (
+               <Marker key={marker.time.toISOString()} position={{ lat: marker.lat, lng: marker.lng }}></Marker>
+            ))}
+         </GoogleMap>
+      ))
+   );
+
+   console.log(`Rendering TestC :`);
+
    return (
       <div className='map-container'>
          <h1>
@@ -46,7 +46,6 @@ function App() {
          </h1>
          <MyMapComponent
             // isMarkerShown
-            setMarkers={setMarkers}
             googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=places`}
             loadingElement={<div className='full-size' />}
             containerElement={<div className='full-size' />}
