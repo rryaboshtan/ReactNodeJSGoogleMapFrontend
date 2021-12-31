ab4772ca1f509bd7c7592eb27eaa6fd522194ace
rryaboshtan <rrvworkroman@gmail.com>
2021-12-31 03:20:54 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '@reach/combobox/styles.css';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-
+import './App.css';
 // const center = {
 //   lat: 43.653225,
 //   lng: -79.383186,
@@ -11,20 +11,20 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 const MyMapComponent = withScriptjs(
    withGoogleMap(props => (
       <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-         {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+         {/* {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />} */}
       </GoogleMap>
    ))
 );
 
 function App() {
    return (
-      <div style={{ width: '100vw', height: '100vh' }}>
+      <div class='map-container'>
          <MyMapComponent
-            isMarkerShown
+            // isMarkerShown
             googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=places`}
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
+            loadingElement={<div class='full-size' />}
+            containerElement={<div class='full-size' />}
+            mapElement={<div class='full-size' />}
          />
       </div>
    );
