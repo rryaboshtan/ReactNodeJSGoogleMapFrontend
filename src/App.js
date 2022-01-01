b494535b7cf8cf7abb261e11240a0da03fe26e1a
rryaboshtan <rrvworkroman@gmail.com>
2022-01-01 04:31:47 +0200

    Test task

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'reac
 import Search from './components/Search';
 import './App.css';
 import '@reach/combobox/styles.css';
+import data from './skateboard-parks.js'
 import mapStyles from './mapStyles';
 
 const options = {
@@ -13,7 +14,7 @@ const options = {
 
 function App() {
    const [markers, setMarkers] = useState([]);
-   const [selected, setSelected] = useState(null);
+   // const [selected, setSelected] = useState(null);
 
    const onMapClick = useCallback(event => {
       setMarkers(current => [
@@ -22,19 +23,20 @@ function App() {
             lat: event.latLng.lat(),
             lng: event.latLng.lng(),
             time: new Date(),
+            icon: '/blueCircle3.png',
          },
       ]);
    }, []);
 
-   const mapRef = useRef();
-   const onMapLoad = useCallback(map => {
-      mapRef.current = map;
-   }, []);
+   // const mapRef = useRef();
+   // const onMapLoad = useCallback(map => {
+   //    mapRef.current = map;
+   // }, []);
 
-   const panTo = useCallback(({ lat, lng }) => {
-      mapRef.current.panTo({ lat, lng });
-      mapRef.current.setZoom(14);
-   }, [mapRef]);
+   // const panTo = useCallback(({ lat, lng }) => {
+   //    mapRef.current.panTo({ lat, lng });
+   //    mapRef.current.setZoom(14);
+   // }, []);
 
    const MyMapComponent = withScriptjs(
       withGoogleMap(() => (
@@ -43,22 +45,40 @@ function App() {
             defaultCenter={{ lat: 43.653225, lng: -79.383186 }}
             options={options}
             onClick={onMapClick}
-            onLoad={onMapLoad}
+            // onLoad={onMapLoad}
          >
-            {markers.map(marker => (
+            {data.features.map(park => (
+                  <Marker
+                     key={park.properties.PARK_ID}
+                     position={{
+                        lat: park.geometry.coordinates[1],
+                        lng: park.geometry.coordinates[0],
+                     }}
+                  >
+
+                  </Marker>
+               ))
+            }
+            {/* {markers.map((marker, outerIndex) => (
                <Marker
                   key={marker.time.toISOString()}
                   position={{ lat: marker.lat, lng: marker.lng }}
                   icon={{
-                     url: '/marker.png',
+                     url: marker.icon,
                   }}
                   onClick={() => {
                      setSelected(marker);
+                     marker.icon = '/orangeCircle.svg';
+                     markers.forEach((marker, index) => {
+                        if (index !== outerIndex) {
+                           marker.icon = '/blueCircle3.png';
+                        }
+                     })
                   }}
                ></Marker>
-            ))}
+            ))} */}
 
-            {selected ? (
+            {/* {selected ? (
                <InfoWindow
                   position={{ lat: selected.lat, lng: selected.lng }}
                   onCloseClick={() => {
@@ -69,7 +89,7 @@ function App() {
                      <h2>Bear spotted!</h2>
                   </div>
                </InfoWindow>
-            ) : null}
+            ) : null} */}
          </GoogleMap>
       ))
    );
@@ -80,13 +100,13 @@ function App() {
             <span>Bears🎪</span>
          </h1>
 
-         <Search panTo={panTo} />
+         {/* <Search panTo={panTo} /> */}
          <MyMapComponent
             googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=places,geometry,drawing`}
             loadingElement={<div className='full-size' />}
             containerElement={<div className='full-size' />}
             mapElement={<div className='full-size' />}
-            onLoad={onMapLoad}
+            // onLoad={onMapLoad}
          />
       </div>
    );
